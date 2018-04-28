18bb6140f4b7a0bcd5b386abaa09b6395c219297
Philip803 <30559200+Philip803@users.noreply.github.com>
2018-04-27 21:42:40 -0400

    refactor the code for cloud firebase db fetch

--- FSA/Controller/MainVC.swift
+++ FSA/Controller/MainVC.swift
@@ -54,40 +54,54 @@ class MainVC: UIViewController , UITableViewDelegate, UITableViewDataSource {
     }
     
     func setListener(){
-        //add listener
-        questionsListener = questionsCollectionRef
-            .whereField(CATEGORY, isEqualTo: selectedCategory)
-            .order(by: TIMESTAMP, descending: true)
-            .addSnapshotListener { (snapshot, error) in
-            if let err = error {
-                debugPrint("Error fetching docs: \(err)")
-            } else {
-                self.questions.removeAll()
-                guard let snap = snapshot else {return}
-                for document in snap.documents {
-                    let data = document.data()
-                    let username = data[USERNAME] as? String ?? "Anonymous"   //default value
-                    let timestamp = data[TIMESTAMP] as? Date ?? Date()
-                    let questionTxt = data[QUESTION_TXT] as? String ?? ""
-                    let numLikes = data[NUM_LIKES] as? Int ?? 0
-                    let numComments = data[NUM_COMMENTS] as? Int ?? 0
-                    let documentId = document.documentID
-                    
-                    let newQuestion = Question(username: username, timestamp: timestamp, questionTxt: questionTxt, numLikes: numLikes, numComments: numComments, documentId: documentId)
-                    self.questions.append(newQuestion)
+
+        if selectedCategory == QuestionCategory.popular.rawValue {
+            questionsListener = questionsCollectionRef
+                .order(by: NUM_LIKES, descending: true)
+                .addSnapshotListener { (snapshot, error) in
+                    if let err = error {
+                        debugPrint("Error fetching docs: \(err)")
+                    } else {
+                        self.questions.removeAll()
+                        self.questions = Question.parseData(snapshot: snapshot)
+                        self.tableView.reloadData()
+                    }
+            }
+        } else {
+            
+            questionsListener = questionsCollectionRef
+                .whereField(CATEGORY, isEqualTo: selectedCategory)
+                .order(by: TIMESTAMP, descending: true)
+                .addSnapshotListener { (snapshot, error) in
+                    if let err = error {
+                        debugPrint("Error fetching docs: \(err)")
+                    } else {
+                        self.questions.removeAll()
+                        guard let snap = snapshot else {return}
+                        for document in snap.documents {
+                            let data = document.data()
+                            let username = data[USERNAME] as? String ?? "Anonymous"   //default value
+                            let timestamp = data[TIMESTAMP] as? Date ?? Date()
+                            let questionTxt = data[QUESTION_TXT] as? String ?? ""
+                            let numLikes = data[NUM_LIKES] as? Int ?? 0
+                            let numComments = data[NUM_COMMENTS] as? Int ?? 0
+                            let documentId = document.documentID
+                            
+                            let newQuestion = Question(username: username, timestamp: timestamp, questionTxt: questionTxt, numLikes: numLikes, numComments: numComments, documentId: documentId)
+                            self.questions.append(newQuestion)
+                        }
+                        self.tableView.reloadData()
                 }
-                self.tableView.reloadData()
             }
         }
+        
     }
     
     //fetch data from db when view appear
     override func viewWillAppear(_ animated: Bool) {
-        
         setListener()
         
-//        questionsCollectionRef.getDocuments { (snapshot, error) in // one time
-//
+//        questionsCollectionRef.getDocuments // one time
     }
 
     //del arr when leave view for best practice

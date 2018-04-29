c30a3a590e957b3ffdc2e1c636749cbe10183c59
Philip_Leung <philip.leung90@gmail.com>
2018-04-29 15:01:16 -0400

    setup camera view on CoreMLVC

--- FSA/Controller/CoreMLVC.swift
+++ FSA/Controller/CoreMLVC.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
-import AVKit
+import AVFoundation
 import Vision
 
 class CoreMLVC: UIViewController {
 
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     @IBOutlet weak var captureImageView: RoundedShadowImageView!
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var flashBtn: RoundedShowButton!
@@ -26,4 +30,41 @@ class CoreMLVC: UIViewController {
         
      }
  
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080  //quality of capture
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            //input
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            
+            //output
+            cameraOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(cameraOutput) == true {
+                captureSession.addOutput(cameraOutput!)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession.startRunning()
+            }
+        } catch {
+            debugPrint(error)
+        }
+    }
+    
 }
